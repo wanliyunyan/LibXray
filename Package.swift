ba91c73b63c3de7675f5e8b9fb8c05c5c8a4ed4e
潘晓升 <wanliyunyan@users.noreply.github.com>
2026-07-31 16:13:45 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LibXray",
-            url:"https://github.com/wanliyunyan/LibXray/releases/download/26.7.11/LibXray.xcframework.zip",
+            url:"https://github.com/wanliyunyan/LibXray/releases/download/26.7.28/LibXray.xcframework.zip",
             checksum: "f5ed1c6bd02522b94328dd641e49341f76a520d0918889518c9fadf4692c9135"
         )
     ]
